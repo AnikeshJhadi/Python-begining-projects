096319fc4d4420f0b73babe8cff12299efb30eaf
AnikeshJhadi <74179827+AnikeshJhadi@users.noreply.github.com>
2021-07-11 19:53:47 +0530

    Implemented tuple methods

---
+++ Tuplemethod.py
@@ -0,0 +1,7 @@
+fruit=("banana","orange","grapes","banana","mango","pineapple","apple","orange","grapes","banana","kiwi")
+print("Fruits:-")
+print(fruit)
+i=input("Enter the fruit whose no. you have to find:-")
+print("No. of fruits in the tuple:-"+str(fruit.count(i)))
+j=input("Enter the fruit whose index you have to find:-")
+print("Index of fruit:-"+str(fruit.index(j)))
